5e2577fdb4e72114518ed038dec676eb106bf44c
unknown <nishmakala@gmail.com>
2025-03-06 10:34:58 -0500

    Created an arrayList breakableBlockRow to manage all breakableBlock objects

--- src/game/KeyGame.java
+++ src/game/KeyGame.java
@@ -1,7 +1,5 @@
 package game;
 import java.awt.Graphics;
-
-
 /*
 CLASS: YourGameNameoids
 DESCRIPTION: Extending Game, YourGameName is all in the paint method.
@@ -11,11 +9,12 @@ NOTE: This class is the metaphorical "main method" of your program,
 */
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 class KeyGame extends Game {
 	static int counter = 0;
-	private BreakableBlocks testBlock, testBlock2;
-	private Key key;
+	private ArrayList <BreakableBlocks> breakableBlockRow = new ArrayList<BreakableBlocks>(); //consists of one row of breakable blocks
+	private Key key; // the key needed to pass level
 	private Point[] breakableBlocksPoints = {new Point(0,0), new Point(0,30), new Point(30,30), new Point(30,0)};
 	private Point[] keyPoints = {new Point(20, 50), new Point(17, 60), new Point(10, 67), new Point(0, 70), new Point(-10, 67), 
 			new Point(-17, 60), new Point(-20, 50), new Point(-17, 40), new Point(-10, 32), 
@@ -26,9 +25,14 @@ class KeyGame extends Game {
     super("KeyGame!!!", 800, 600);
     this.setFocusable(true);
 	this.requestFocus();
-	testBlock = new BreakableBlocks(breakableBlocksPoints, 50, 500);
-	testBlock2 = new BreakableBlocks(breakableBlocksPoints, 90, 500);
-	key = new Key(keyPoints);
+	
+	for (int i = 1; i <= 4; i++) { //draws blocks in a single row 40 spaces apart
+		breakableBlockRow.add(new BreakableBlocks(breakableBlocksPoints, 80+(40*i), 500));
+		breakableBlockRow.add(new BreakableBlocks(breakableBlocksPoints, 200+(40*i), 400));
+		breakableBlockRow.add(new BreakableBlocks(breakableBlocksPoints, 300+(40*i), 300));
+	}
+
+	key = new Key(keyPoints); 
 	
   }
   
@@ -36,8 +40,10 @@ class KeyGame extends Game {
     	brush.setColor(Color.black);
     	brush.fillRect(0,0,width,height);
     	
-    	testBlock.paint(brush);
-    	testBlock2.paint(brush);
+    	for (int index = 0; index < breakableBlockRow.size(); index++) {
+    		breakableBlockRow.get(index).paint(brush);
+    	}
+    	
     	key.paint(brush);
 
     	
